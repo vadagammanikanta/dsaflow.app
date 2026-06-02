2f7811e56de6881c6aeb7cce4ca479644489b832
vadagammanikanta <vadagammanikanta2006@gmail.com>
2026-06-02 22:37:01 +0530

    chore: rename name placeholder from Aniketh Reddy to Ganesh

--- src/App.jsx
+++ src/App.jsx
@@ -355,7 +355,7 @@ function AppLayout() {
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><circle cx="12" cy="8" r="4"/><path d="M20 21a8 8 0 1 0-16 0"/></svg>
                     <input 
                       type="text" 
-                      placeholder="Aniketh Reddy" 
+                      placeholder="Ganesh" 
                       value={signUpName} 
                       onChange={e => setSignUpName(e.target.value)} 
                       required 
