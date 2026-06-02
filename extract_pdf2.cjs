18331d333ba41fd91653d4d2b99344da0638f157
vadagammanikanta <vadagammanikanta2006@gmail.com>
2026-06-02 21:43:57 +0530

    chore: ignore and untrack utility scripts, raw PDF, local data files, and test modules

--- extract_pdf2.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const PDFParser = require('pdf2json');
-let pdfParser = new PDFParser(this, 1);
-pdfParser.on('pdfParser_dataError', errData => console.error(errData.parserError));
-pdfParser.on('pdfParser_dataReady', pdfData => {
-    fs.writeFileSync('dsa_textbook.txt', pdfParser.getRawTextContent());
-    console.log('Extracted successfully');
-});
-pdfParser.loadPDF('Dsa.pdf');
